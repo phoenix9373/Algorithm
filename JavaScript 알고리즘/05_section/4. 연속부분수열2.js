b20dd53f65dfd3a66a36cf984f7ad2151978e438
Jinwoo Yu <phoenix9373@naver.com>
2021-10-15 08:35:08 +0900

    📝 Feat: Solved JS section05/4.연속부분수열2

--- JavaScript 알고리즘/05_section/4. 연속부분수열2.js
+++ JavaScript 알고리즘/05_section/4. 연속부분수열2.js
@@ -1,4 +1,33 @@
-function solution(m, arr) {}
+function solution(m, arr) {
+  let i = 0;
+  let j = 0;
+  let cnt = 0;
+  let sum;
+
+  while (i < arr.length) {
+    if (i === j) {
+      sum = arr[i];
+    }
+
+    if (sum > m) {
+      i++;
+      j = i;
+    }
+
+    while (sum <= m) {
+      cnt++;
+      j++;
+      if (j < arr.length) sum += arr[j];
+      if (j >= arr.length) {
+        i++;
+        j = i;
+        break;
+      }
+    }
+  }
+
+  return cnt;
+}
 
 let a = [1, 3, 1, 2, 3];
 console.log(solution(5, a));
